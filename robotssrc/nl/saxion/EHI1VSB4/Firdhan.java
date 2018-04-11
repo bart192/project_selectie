d468ce799a190290a9f7a47e710cf705a3e97bd2
Bart van Es <bartvanes@home.nl>
2018-04-11 16:05:32 +0200

    Fix

---
+++ robotssrc/nl/saxion/EHI1VSB4/Firdhan.java
@@ -0,0 +1,10 @@
+package nl.saxion.EHI1VSB4;
+
+import nl.saxion.EHI1VSB4.Models.BoxingBot;
+
+public class Firdhan extends BoxingBot {
+    @Override
+    public void run() {
+        super.run();
+    }
+}
---
+++ robotssrc/nl/saxion/EHI1VSB4/Kelvin.java
@@ -0,0 +1,11 @@
+package nl.saxion.EHI1VSB4;
+
+import nl.saxion.EHI1VSB4.Models.BoxingBot;
+
+public class Kelvin extends BoxingBot {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
